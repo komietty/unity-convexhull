9fb2cfaeb98cdf3addfb7e3d0ac1454315ee9f7c
sakikomikado <komietty@gmail.com>
2021-06-15 14:26:13 +0900

    update 3d convex (at least works)

--- Assets/Demo/Test3D.cs
+++ Assets/Demo/Test3D.cs
@@ -10,13 +10,15 @@ public class Test3D : MonoBehaviour {
     [SerializeField, Range(0, 100)] protected int num;
     [SerializeField, Range(0, 2)] protected float dst;
     [SerializeField] protected bool showNormal;
+    [SerializeField] protected bool showOthers;
+    [SerializeField, Range(0, 10)] protected int currId;
     Convex convex;
 
     void Start() {
         for (int i = 0; i < num; i++) {
             var g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             g.transform.localScale *= 0.1f;
-            g.transform.position = UnityEngine.Random.insideUnitSphere * dst;
+            g.transform.position = UnityEngine.Random.onUnitSphere * dst;
             g.transform.SetParent(this.transform);
         }
     }
@@ -40,16 +42,34 @@ public class Test3D : MonoBehaviour {
     void OnDrawGizmos() {
         if (!Application.isPlaying) return;
         var nodes = convex.nodes;
+        if (showOthers) {
+            for (int i = 0; i < nodes.Count; i++) {
+                var n = nodes[i];
+                Gizmos.DrawLine((f3)n.a, (f3)n.b);
+                Gizmos.DrawLine((f3)n.b, (f3)n.c);
+                Gizmos.DrawLine((f3)n.c, (f3)n.a);
+                if (showNormal) {
+                    Gizmos.DrawLine(
+                        (f3)n.t.GetGravityCenter(),
+                        (f3)n.t.GetGravityCenter() + (f3)n.normal * 0.5f
+                    );
+                }
+            }
+        }
+
         for (int i = 0; i < nodes.Count; i++) {
             var n = nodes[i];
-            Gizmos.DrawLine((f3)n.a, (f3)n.b);
-            Gizmos.DrawLine((f3)n.b, (f3)n.c);
-            Gizmos.DrawLine((f3)n.c, (f3)n.a);
-            if (showNormal) {
-                Gizmos.DrawLine(
-                    (f3)n.t.GetGravityCenter(),
-                    (f3)n.t.GetGravityCenter() + (f3)n.normal * 0.5f
-                );
+            if (i == currId && n.neighbors.Count > 3) {
+                Gizmos.color = Color.cyan;
+                n.neighbors.ForEach(nei => {
+                    Gizmos.DrawLine((f3)nei.a, (f3)nei.b);
+                    Gizmos.DrawLine((f3)nei.b, (f3)nei.c);
+                    Gizmos.DrawLine((f3)nei.c, (f3)nei.a);
+                });
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine((f3)n.a, (f3)n.b);
+                Gizmos.DrawLine((f3)n.b, (f3)n.c);
+                Gizmos.DrawLine((f3)n.c, (f3)n.a);
             }
         }
     }
